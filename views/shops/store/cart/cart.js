06d067521942045fbf649d279215f9274e931f52
irthos <irth03@gmail.com>
2014-03-11 12:59:09 -0700

    cart-where are ye?

---
+++ views/shops/store/cart/cart.js
@@ -0,0 +1,12 @@
+angular.module('ecoposApp').directive('cart', function() {
+	return {
+		restrict: 'E',
+		replace: true,
+
+		templateUrl: 'views/shops/store/cart/cart.html',
+		link: function(scope, element, attrs, fn) {
+
+
+		}
+	};
+});
