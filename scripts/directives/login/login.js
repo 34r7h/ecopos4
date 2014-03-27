550a88815b186fc64ed857f446c6310da42863c3
irthos <irth03@gmail.com>
2014-03-27 16:03:42 -0700

    loginDir

---
+++ scripts/directives/login/login.js
@@ -0,0 +1,14 @@
+angular.module('ecoposApp').directive('login', function() {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+
+		},
+		templateUrl: 'scripts/directives/login/login.html',
+		link: function(scope, element, attrs, fn) {
+
+
+		}
+	};
+});
