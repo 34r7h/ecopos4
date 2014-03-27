b81041d24fb4737cd76a376f511b7924db32e1f4
Pat Long <plong00@gmail.com>
2014-03-27 16:54:28 -0700

    moved events logic from MainCtrl into events directive

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -20,43 +20,4 @@ angular.module('ecoposApp')
 			}
 		});
 
-		$scope.newEvent = {
-			title: '',
-			description: '',
-			users: [],
-			shiftType: {},
-			type: 'todo',
-			date: new Date(),
-			end: new Date(),
-			hasEnd: false,
-			dueDate: false
-		};
-
-		$scope.createEvent = function(){
-			var users = {};
-			/**
-			 if($scope.user && $scope.user.id){
-                users[$scope.user.id] = true;
-            }
-			 */
-			angular.forEach($scope.newEvent.users, function(username, index){
-				users[username] = $scope.newEvent.type;
-				if($scope.newEvent.shiftType[username]){
-					users[username] += ':'+$scope.newEvent.shiftType[username];
-				}
-			});
-
-			var dateStamp = 0;
-			var endStamp = 0;
-			if($scope.newEvent.type === 'calendar' || $scope.newEvent.type === 'shift' || ($scope.newEvent.type === 'todo' && $scope.newEvent.dueDate)){
-				dateStamp = $scope.newEvent.date.getTime();
-				if($scope.newEvent.type === 'shift' || $scope.newEvent.hasEnd){
-					endStamp = $scope.newEvent.end.getTime();
-				}
-			}
-
-			// TODO: need some validation here (or in the system.createEvent as a promise)
-			system.createEvent($scope.newEvent.title, $scope.newEvent.description, users, $scope.newEvent.type, dateStamp, endStamp);
-		};
-
 	});
