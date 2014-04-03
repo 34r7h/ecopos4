29ec75ad894fa7d7731251ecac10fb26e7f3ddd4
irthos <irth03@gmail.com>
2014-04-03 10:46:46 -0700

    infinite scroll and beyond

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,5 +1,25 @@
 angular.module('ecoposApp')
 	.controller('MainCtrl', function ($rootScope, $scope, $log, $state, $timeout, syncData, system, firebaseRef) {
+
+		$scope.addItems = function () {
+			for (var i = 0; i < 10; i++) {
+				$scope.items.push({name:'item ' + ($scope.items.length + 1)});
+
+				if ($scope.items.length >= $scope.maxItems) {
+					$scope.canLoad = false;
+					return;
+				}
+			}
+		};
+
+		$scope.reset = function () {
+			$scope.items = [];
+			$scope.canLoad = true;
+			$scope.addItems();
+		};
+
+		$scope.reset();
+
 		$scope.iconz = {
 			icon1:{
 				icon:"fa fa-plus",
