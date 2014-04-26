643fb84e368bdcd173eb9e944c25e6d2cd29a100
Pat Long <plong00@gmail.com>
2014-04-25 18:52:32 -0700

    ya userInfo comin through

--- app/directives/components/userInfo/userInfo.js
+++ app/directives/components/userInfo/userInfo.js
@@ -1,19 +1,19 @@
-angular.module('ecoposApp').directive('userInfo', function(system) {
+angular.module('ecoposApp').directive('userInfo', function(system, $rootScope, $log, $state) {
 	return {
 		restrict: 'E',
 		replace: true,
 
 		templateUrl: 'app/directives/components/userInfo/userInfo.html',
-		link: function(scope, element, attrs, fn, $rootScope, $log) {
-/**
+		link: function(scope, element, attrs, fn) {
+
 			scope.$watch('user.activeRole', function(value){
 				if(value){
 					// don't reload state if they just logged in - routesecurity is taking care of that
-					if(!$scope.user.session.firstActiveRole){
+					if(!scope.user.session.firstActiveRole){
 						$state.go('ecoApp.nav.not.tools.settings');
 					}
 					else{
-						$scope.user.session.firstActiveRole = false;
+						scope.user.session.firstActiveRole = false;
 					}
 				}
 			});
@@ -21,6 +21,8 @@ angular.module('ecoposApp').directive('userInfo', function(system) {
 			scope.$on('$simpleLogin:profile:loaded', function(event, user){
 				system.api.setUser(user);
 				system.api.setUserActiveRole();
+
+                $state.go('.', null, {reload:true});
 
 				$rootScope.toggle('overlay', 'off');
 				$rootScope.$broadcast('ecopos:user:bound', user);
@@ -38,7 +40,7 @@ angular.module('ecoposApp').directive('userInfo', function(system) {
 			scope.$on('$firebaseSimpleLogin:error', function(event, error){
 				$log.error('simpleLogin:error:'+error);
 			});
- */
+
 
 		}
 	};
