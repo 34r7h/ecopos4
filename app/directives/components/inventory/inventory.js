d7c10e11b8adfb6525afcd37a28f8e518fe45adc
Pat Long <plong00@gmail.com>
2014-05-01 16:12:17 -0700

    inventory import skeleton

--- app/directives/components/inventory/inventory.js
+++ app/directives/components/inventory/inventory.js
@@ -1,10 +1,53 @@
-angular.module('ecoposApp').directive('inventory', function() {
+angular.module('ecoposApp').directive('inventory', function($q, $log, $timeout, system) {
 	return {
 		restrict: 'E',
 		replace: true,
 		scope:'@',
 		templateUrl: 'app/directives/components/inventory/inventory.html',
 		link: function(scope, element, attrs, fn) {
+
+            scope.importHistory = new Array();
+
+            scope.startImport = function(){
+                if(!scope.importing && scope.import && scope.importers[scope.import] && typeof scope.importers[scope.import].import === 'function'){
+                    scope.importing = {import: scope.importers[scope.import], startTime: system.api.currentTime()};
+                    $log.debug('importing \''+scope.importers[scope.import].name+'\' from \''+scope.importers[scope.import]['raw-table']+'\'');
+
+                    scope.importers[scope.import].import().then(function(){
+                        scope.importing.finishTime = system.api.currentTime();
+                        scope.importHistory.push({name: scope.importing.import.name, start: scope.importing.startTime, finish: scope.importing.finishTime});
+                        scope.importing = false;
+                    });
+                }
+            };
+
+            scope.importing = false;
+            scope.import = '';
+            scope.importers = {
+                'inventory': {
+                    name: 'Inventory Sheet',
+                    'raw-table': 'inventory-apr19',
+                    import: function(){
+                        var defer = $q.defer();
+
+                        $timeout(function(){ defer.resolve(); }, 1000);
+
+                        return defer.promise;
+                    }
+                },
+                'horizon': {
+                    'name': 'Horizon',
+                    'raw-table': 'horizon-products',
+                    import: function(){
+                        var defer = $q.defer();
+
+                        $timeout(function(){ defer.resolve(); }, 2567);
+
+                        return defer.promise;
+                    }
+                }
+            };
+
 		}
 	};
 });
