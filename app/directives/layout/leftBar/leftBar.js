f301c90407fe1f25593f81190363d6bfbcd95f80
irthos <irth03@gmail.com>
2014-05-11 01:35:05 -0700

    fn with sideouts

--- app/directives/layout/leftBar/leftBar.js
+++ app/directives/layout/leftBar/leftBar.js
@@ -12,12 +12,10 @@ angular.module('ecoposApp').directive('leftBar', function(shop, system) {
 	.directive('leftSize', function(shop, system, $compile) {
 		return {
 			restrict: 'E',
-			replace: true,
 			template: '<div>' +
-				'<button class="col-xs-4" ng-click="right=false;left=false;leftmd=false; leftsm=!leftsm" ng-model="left"></button>'+
-				'<button class="col-xs-4" ng-click="right=false;rightmd=false;left=false;leftsm=false; leftmd=!leftmd" ng-model="left"></button>'+
-				'<button class="col-xs-4" ng-click="right=false;rightmd=false;rightsm=false;leftmd=false;leftsm=false; left=!left" ng-model="left"></button>' +
-
+				'<button class="col-xs-3" ng-click="right=false;left=false;leftmd=false; leftsm=!leftsm" ng-model="leftsm"></button>'+
+				'<button class="col-xs-3" ng-click="right=false;rightmd=false;left=false;leftsm=false; leftmd=!leftmd" ng-model="leftmd"></button>'+
+				'<button class="col-xs-3" ng-click="right=false;rightmd=false;rightsm=false;leftmd=false;leftsm=false; left=!left" ng-model="left"></button>' +
 				'</div>',
 			link: function(iElem, scope, element, attrs, fn) {
 
@@ -28,34 +26,13 @@ angular.module('ecoposApp').directive('leftBar', function(shop, system) {
 	.directive('rightSize', function(shop, system, $compile) {
 		return {
 			restrict: 'E',
-			replace: true,
 			template: '<div>' +
-				'<button class="col-xs-4" ng-click="rightBig" ng-model="right"></button>' +
-				'<button class="col-xs-4" ng-click="rightMed" ng-model="right"></button>'+
-				'<button class="col-xs-4" ng-click="rightSm" ng-model="right"></button>'+
+				'<button class="col-xs-3" ng-click="rightmd=false;rightsm=false;leftmd=false;leftsm=false; left=false; right=!right;"></button>' +
+				'<button class="col-xs-3" ng-click="right=false;rightmd=false;rightsm=false;leftmd=false;left=false; rightmd=!rightmd;"></button>'+
+				'<button class="col-xs-3" ng-click="right=false;rightsm=false;leftmd=false;leftsm=false;left=false;rightsm=!rightsm;"></button>'+
+				'<button class="col-xs-3" ng-click="right=false;rightmd=false;rightsm=false;leftmd=false;leftsm=false; left=false"></button>' +
 				'</div>',
-			link: function(iElem, $scope, element, attrs, fn) {
-				$scope.rightBig = function($scope){
-					$scope.left=false;
-					$scope.leftmd=false;
-					$scope.leftsm=false;
-					$scope.rightmd=false;
-					$scope.rightsm=false;
-					$scope.right=!$scope.right;
-				};
-				$scope.rightMed = function($scope){
-					$scope.left=false;
-					$scope.leftmd=false;
-					$scope.right=false;
-					$scope.rightsm=false;
-					$scope.rightmd=!$scope.rightmd;
-				};
-				$scope.rightSm = function($scope){
-					$scope.left=false;
-					$scope.right=false;
-					$scope.rightmd=false;
-					$scope.rightsm=!$scope.rightsm
-				}
+			link: function(iElem, scope, element, attrs, fn) {
 
 
 			}
