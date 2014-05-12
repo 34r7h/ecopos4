90c9deae9282c1960aaa1bdb9de9752017628a6c
Pat Long <plong00@gmail.com>
2014-05-11 17:40:19 -0700

    breadcrumb scoping proper from shop through MainCtrl and into breadcrumb directive

--- app/directives/components/breadcrumb/breadcrumb.js
+++ app/directives/components/breadcrumb/breadcrumb.js
@@ -2,9 +2,6 @@ angular.module('ecoposApp').directive('breadcrumb', function() {
 	return {
 		restrict: 'E',
 		replace: true,
-		scope: {
-
-		},
 		templateUrl: 'app/directives/components/breadcrumb/breadcrumb.html',
 		link: function(scope, element, attrs, fn) {
 
