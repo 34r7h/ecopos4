299d08913976c57c1d698658f648a5925f2fb464
Pat Long <plong00@gmail.com>
2014-05-15 18:18:17 -0700

    new ecoFilter :)

---
+++ app/filters/ecoFilter-spec.js
@@ -0,0 +1,13 @@
+describe('ecoFilter', function() {
+
+    beforeEach(module('ecoposApp'));
+
+    it('should ...', inject(function($filter) {
+
+        var filter = $filter('ecoFilter');
+
+        expect(filter('input')).toEqual('output');
+
+    }));
+
+});
