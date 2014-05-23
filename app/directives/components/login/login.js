ca72ed3632b6645ad5931618baec0b8f56d12495
Pat Long <plong00@gmail.com>
2014-05-22 17:10:40 -0700

    taking out the userInfo directive

--- app/directives/components/login/login.js
+++ app/directives/components/login/login.js
@@ -1,4 +1,4 @@
-angular.module('ecoposApp').directive('login', function(system, simpleLogin, profileManager, $rootScope, syncData, $log) {
+angular.module('ecoposApp').directive('login', function(system, simpleLogin, profileManager, $rootScope, syncData, $state, shop, $log) {
 	return {
 		restrict: 'E',
 		replace: true,
@@ -198,6 +198,47 @@ angular.module('ecoposApp').directive('login', function(system, simpleLogin, pro
                     scope.err = err;
                 });
             };
+
+
+            scope.$watch('user.activeRole', function(value){
+                if(value){
+                    // don't reload state if they just logged in - routesecurity is taking care of that
+                    if(!scope.user.session.firstActiveRole){
+                        $state.go('ecoApp.nav.not.tools.settings');
+                    }
+                    else{
+                        scope.user.session.firstActiveRole = false;
+                    }
+                }
+            });
+
+            scope.$on('$simpleLogin:profile:loaded', function(event, user){
+                console.log('profile loaded');
+                system.api.setUser(user);
+                system.api.setUserActiveRole();
+                shop.api.loadOrder(user.activeOrder, true);
+
+                var theState = $state.get('ecoApp.nav.not.tools.settings');
+                theState.reloadOnSearch = true;
+                $state.go('.', null, {reload:true}).then(function(){
+                    theState.reloadOnSearch = false;
+                });
+
+                $rootScope.$broadcast('ecopos:user:bound', user);
+
+                system.api.startUserSession();
+                system.api.loadUserData();
+            });
+
+            scope.$on('$firebaseSimpleLogin:logout', function(event){
+                $log.debug('simpleLogin:logout:');
+                system.api.endUserSession();
+                system.api.setUser(null);
+            });
+
+            scope.$on('$firebaseSimpleLogin:error', function(event, error){
+                $log.error('simpleLogin:error:'+error);
+            });
         }
 	};
 });
