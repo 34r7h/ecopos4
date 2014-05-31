6541b4634509f25ccfaa7812af8977fb2d27d7db
Pat Long <plong00@gmail.com>
2014-05-31 09:09:46 -0700

    loading in user,employee,manager orders

--- app/directives/components/orders/orders.js
+++ app/directives/components/orders/orders.js
@@ -1,11 +1,32 @@
-angular.module('ecoposApp').directive('orders', function($filter, $q, ngTableParams, shop) {
+angular.module('ecoposApp').directive('orders', function($filter, $q, ngTableParams, system, shop) {
 	return {
 		restrict: 'E',
 		replace: true,
 
 		templateUrl: 'app/directives/components/orders/orders.html',
-		link: function($scope, scope, element, attrs, fn, syncData) {
-			$scope.total = shop.api.cartTotal;
+		link: function(scope, element, attrs) {
+			scope.total = shop.api.cartTotal;
+
+            scope.orders = [];
+            scope.userOrders = system.data.user.orders;
+            scope.employeeOrders = system.data.employee.orders;
+            scope.managerOrders = system.data.manager.orders;
+
+            scope.combineOrders = function(){
+                scope.orders = [];
+                if(scope.userOrders){
+                    scope.orders = scope.orders.concat($filter('orderByPriority')(scope.userOrders));
+                }
+                if(scope.employeeOrders){
+                    scope.orders = scope.orders.concat($filter('orderByPriority')(scope.employeeOrders));
+                }
+                if(scope.managerOrders){
+                    scope.orders = scope.orders.concat($filter('orderByPriority')(scope.managerOrders));
+                }
+            };
+            scope.$watchCollection('userOrders', scope.combineOrders);
+            scope.$watchCollection('employeeOrders', scope.combineOrders);
+            scope.$watchCollection('managerOrders', scope.combineOrders);
 		}
 	};
 });
