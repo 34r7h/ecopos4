86951d917d1ccb4a8128328e1c0d647197511466
Pat Long <plong00@gmail.com>
2014-05-31 17:49:09 -0700

    tightening up the tree traversal

--- app/directives/components/selectTree/selectTree.js
+++ app/directives/components/selectTree/selectTree.js
@@ -1,4 +1,4 @@
-angular.module("ecoposApp").directive("selectTree", function(){
+angular.module("ecoposApp").directive("selectTree", function($filter){
         return {
             restrict: "E",
             scope:{tree:"=source",path:"=ngModel",id:'@',size:'@',maxSize:'@'},
@@ -7,10 +7,24 @@ angular.module("ecoposApp").directive("selectTree", function(){
                 if(!scope.path){
                     scope.path = [];
                 }
+                scope.hasChildren = function(item){
+                    return (item.name && item.children);
+                };
+                scope.hasGrandChildren = function(item){
+                    var result = false;
+                    if(item && item.children){
+                        angular.forEach(item.children, function(childItem, childIdx){
+                            if(childItem.children){
+                                result = true;
+                            }
+                        });
+                    }
+                    return result;
+                };
                 scope.selectSize = function(forItem){
                     var result = scope.size?scope.size:4;
                     if(!scope.size){
-                        result = (forItem && forItem.children)?(Object.keys(forItem.children).length+1):1;
+                        result = (forItem && forItem.children)?(($filter('filter')($filter('orderByPriority')(forItem.children), scope.hasChildren)).length+1):1;
                     }
                     return (scope.maxSize && result > scope.maxSize)?scope.maxSize:result;
                 };
@@ -22,7 +36,7 @@ angular.module("ecoposApp").directive("selectTree", function(){
                         scope.path = scope.path.slice(0, level+1);
                     }
 
-                    // this is the magic the updates the recursive ng-include scope
+                    // this is the magic that updates the recursive ng-include scope
                     var currElement = angular.element(document.getElementById(scope.id+'-'+level));
                     var nextElement = angular.element(document.getElementById(scope.id+'-'+(level+1)));
                     if(nextElement && currElement && nextElement.scope() && currElement.scope()){
