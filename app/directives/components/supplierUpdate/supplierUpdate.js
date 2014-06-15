12ac8ae43ff2ab79e801943962639e76ee9aaa70
Pat Long <plong00@gmail.com>
2014-06-14 17:34:27 -0700

    skeleton of csv file selection and reading

--- app/directives/components/supplierUpdate/supplierUpdate.js
+++ app/directives/components/supplierUpdate/supplierUpdate.js
@@ -5,6 +5,7 @@ angular.module('ecoposApp').directive('supplierUpdate', function(imports) {
 		templateUrl: 'app/directives/components/supplierUpdate/supplierUpdate.html',
 		link: function(scope, element, attrs, fn) {
             scope.imports = imports.data;
+            scope.import = imports.api.import;
 		}
 	};
 });
--- app/services/imports.js
+++ app/services/imports.js
@@ -4,8 +4,50 @@ angular.module('ecoposApp').factory('imports',function(syncData){
     var api = {
         loadConfigs: function(){
             data.config = syncData('imports/config');
+        },
+
+        import: function(){
+            if(data.sourceFile){
+                console.log('importing from '+JSON.stringify(data.sourceFile));
+                switch(data.sourceFile.type){
+                    case 'text/csv':
+                        if(!angular.isArray(data.sourceFile)){
+                            api.importCSV(data.sourceFile);
+                        }
+                        break;
+                    default:
+                        data.errors = ['Unrecognized source file format'];
+                        break;
+                }
+            }
+            else{
+                data.errors = ['Please select a source file for import'];
+            }
+
+        },
+
+        importCSV: function(csvFile){
+            if(angular.isDefined(window.FileReader)){
+                console.log('csv import from '+csvFile.name);
+                var fileReader = new FileReader();
+                fileReader.readAsText(csvFile);
+                fileReader.onloadend = function(event){
+                    if(event.srcElement.result){
+                        var csvData = event.srcElement.result;
+
+                        // ecodocs: parse the csvData - it's a string at this point
+
+                        console.log('loadend:'+event.loaded+'/'+event.total);
+                    }
+                };
+
+            }
+            else{
+                data.errors = ['Browser does not support file reading'];
+            }
         }
     };
+
 
     var imports = {
         api: api,
